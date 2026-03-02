7db4c28c2fb19d8610bb9ee9cf5db5be46f7744a
JAOrtiz19 <ic.jortiz@gmail.com>
2026-03-02 11:30:54 -0600

    Se actualizo la url del servidor web

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@
 // Descomenta la que quieras usar
 
 // Local
-const API_BASE_URL = 'http://localhost:8080';
+// const API_BASE_URL = 'http://localhost:8080';
 
 // Producción (remoto)
-// const API_BASE_URL = 'https://liaaproyectos.uaz.edu.mx/appturistica';
+const API_BASE_URL = 'https://liaaproyectos.uaz.edu.mx/appturistica';
 
 export default API_BASE_URL;
